24d4a2c479773c41e1b802b24b2159fa3af454ac
Joel Joseph <joel.joseph211@gmail.com>
2021-09-21 14:13:52 +0530

    Done with forEach

---
+++ foreach_js.js
@@ -0,0 +1,9 @@
+const arr = [1,2,3,4,5];
+
+const understandForEach= (arr)=>{
+    // read more about filter --> https://www.w3schools.com/jsref/jsref_foreach.asp
+    arr.forEach(item=>console.log(item +1 ))
+
+}
+
+understandForEach(arr)
